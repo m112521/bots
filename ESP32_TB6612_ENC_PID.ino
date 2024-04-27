52189977ad53827dfdaec96af04c9b9eb6f78ce1
Azamat Tavitov <85460283+m112521@users.noreply.github.com>
2024-04-27 20:08:07 +0300

    add actual pid

--- ESP32_TB6612_ENC_PID.ino
+++ ESP32_TB6612_ENC_PID.ino
@@ -1,6 +1,9 @@
 // #include <Arduino.h>
 #include <TB6612_ESP32.h>
-
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Arduino_JSON.h>
 
 #define ENCA1 18 // YELLOW
 #define ENCA2 19 // WHITE
@@ -16,26 +19,13 @@
 #define PWMB 25 // ESP32 Pin D25 to TB6612FNG Pin PWMB
 #define STBY 33 // ESP32 Pin D33 to TB6612FNG Pin STBY
 
+const char* ssid = "GalaxyAzamat";
+const char* password = "vhrg8328";
 
-// *** Wiring connections from TB6612FNG Motor Controller to the Motors ***
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
-
-// Connect the leads from your motors, don't worry if you don't get them correct the first time
-// Motor 1 should go to A01 and A02
-// Motor 2 should go to B01 and B02
-// If the motors are NOT turning in the right direction reverse the pins (for example the A01 and A02) pins
-// If the left and right motors are reversed then swap the A01 and A02 wires to the B01 and B02 pins 
-
-
-// *** Wiring connections from the Battery Pack to the ESP32 and TB6612FNG Motor Controller ***
-
-
-// A 4 Cell battery pack was used to supply 6 Volts of DC current to the robot and to the ESP32
-// A wire splitter was used to connect the Positive (+) side of the Battery pack to the VIN pin of the ESP32,
-// and to the VM pin of the TB6612FNG Motor Controller.
-// Another wire splitter was used to connect the Negative (-) side of the Battery pack to the GND pin of the ESP32,
-// and to the GND pin of the TB6612FNG Motor Controller
-
+JSONVar readings;
 
 // these constants are used to allow you to make your motor configuration
 // line up with function names like forward.  Value can be 1 or -1
@@ -47,15 +37,96 @@ Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY, 5000, 8, 2);
 
 volatile int posiA1 = 0;
 volatile int posiB1 = 0;
-// Initializing motors.  The library will allow you to initialize as many
-// motors as you have memory for.  If you are using functions like forward
-// that take 2 motors as arguements you can either write new functions or
-// call the function more than once.
 
 long prevT = 0;
 float eprev = 0;
+float eprev2 = 0;
 float eintegral = 0;
 
+int target = 0;
+
+double kp = 1;
+double kd = 0.025;
+double ki = 0.0;
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<p>Target=<span id="s1"></span></p>
+<input id="target" type="number"/>
+<hr/>
+<p>Current=<span id="s5"></span></p>
+<hr/>
+<p>P=<span id="s2"></span></p>
+<input id="p-textbox" type="number" value="1"/>
+<hr/>
+<p>D=<span id="s3"></span></p>
+<input id="d-textbox" type="number" value="0.025"/>
+<hr/>
+<p>I=<span id="s4"></span></p>
+<input id="i-textbox" type="number" value="0.0"/>
+
+<!-- <input id="slider" type="range" min="0" max="255"/> -->
+<button id="yo">SET</button>
+<script>
+let gateway = `ws://${window.location.hostname}/ws`;
+let target = document.querySelector("#target");
+let kp = document.querySelector("#p-textbox");
+let kd = document.querySelector("#d-textbox");
+let ki = document.querySelector("#i-textbox");
+
+let websocket;
+window.addEventListener('load', onload);
+
+function onload(event) {
+    initWebSocket();
+    initButtons();
+}
+
+function initButtons() {
+  document.getElementById('yo').addEventListener('click', forwardMove);
+}
+
+function forwardMove(){
+  //websocket.send('f');
+  console.log(parseFloat(ki.value));
+  websocket.send(JSON.stringify({target: parseInt(target.value),kp:parseInt(kp.value),kd:parseFloat(kd.value),ki:parseFloat(ki.value)}));
+}
+
+function getReadings(){
+    websocket.send("getReadings");
+}
+
+function initWebSocket() {
+    console.log('Trying to open a WebSocket connection…');
+    websocket = new WebSocket(gateway);
+    websocket.onopen = onOpen;
+    websocket.onclose = onClose;
+    websocket.onmessage = onMessage;
+}
+
+function onOpen(event) {
+    console.log('Connection opened');
+    getReadings();
+}
+
+function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+}
+
+function onMessage(event) {
+    console.log(event.data);
+    let myObj = JSON.parse(event.data);
+    let keys = Object.keys(myObj);
+
+    for (let i = 0; i < keys.length; i++){
+        let key = keys[i];
+        document.getElementById(key).innerHTML = myObj[key];
+    }
+    websocket.send("getReadings");
+}</script>
+)rawliteral";
 
 void IRAM_ATTR ISRA1(){
   int b = digitalRead(ENCA2);
@@ -77,19 +148,73 @@ void IRAM_ATTR ISRB1(){
   }
 }
 
+String getSensorReadings(){
+  // int val = analogRead(ptrPin);
+  // Serial.println(val);
+  readings["s1"] = String(target);
+  readings["s2"] =  String(kp);
+  readings["s3"] = String(kd);
+  readings["s4"] = String(ki);
+  readings["s5"] = String(posiA1);
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
 
-void testRun() {
-  forward(motor1, motor2, 255); 
-  delay(2000);
-  forward(motor1, motor2, 0);
-  delay(2000);
-  Serial.println(String(posiA1) + " " + String(posiB1));
-  
-  back(motor1, motor2, -255);
-  delay(2000);
-  back(motor1, motor2, 0);
-  delay(2000);  
-  Serial.println(String(posiA1) + " " + String(posiB1));
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void notifyClients(String sensorReadings) {
+  ws.textAll(sensorReadings);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    
+    JSONVar myObject = JSON.parse((const char*)data);
+    if (myObject.hasOwnProperty("target")) {
+      target = (int)myObject["target"];
+      kp = (int)myObject["kp"];
+      ki = (double)myObject["ki"];
+      kd = (double)myObject["kd"];
+      // Serial.print("myObject[\"target\"] = ");
+      // ledcWrite(ledChannel, brightness);
+    }
+
+    String sensorReadings = getSensorReadings();
+    //Serial.println(sensorReadings);
+    notifyClients(sensorReadings);
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
 }
 
 void setup()
@@ -103,42 +228,35 @@ void setup()
 
   attachInterrupt(ENCA1, ISRA1, RISING);
   attachInterrupt(ENCB1, ISRB1, RISING);
+  //testRun();
 
-  delay(2000);
-  testRun();
-
-  Serial.println("target pos");
+  initWiFi();
+  initWebSocket();
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html);
+  });
+  server.begin();
 }
 
-vois calPID() {
-  // set target position
-  //int target = 1200;
-  int target = 325*sin(prevT/1e6);
-
-  // PID constants
-  float kp = 1;
-  float kd = 0.025;
-  float ki = 0.0;
-
+void loop()
+{
+  //int target = 325*sin(prevT/1e6);
   // time difference
   long currT = micros();
   float deltaT = ((float) (currT - prevT))/( 1.0e6 );
   prevT = currT;
 
-  int pos = 0;
+  int posA1 = 0;
   //ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    pos = posi;
+    posA1 = posiA1;
   //}
 
   // error
-  int e = pos - target;
-
+  int e = posA1 - target;
   // derivative
   float dedt = (e-eprev)/(deltaT);
-
   // integral
   eintegral = eintegral + e*deltaT;
-
   // control signal
   float u = kp*e + kd*dedt + ki*eintegral;
 
@@ -154,58 +272,21 @@ vois calPID() {
     dir = -1;
   }
 
-  // signal the motor
-  setMotor(dir,pwr,PWM,IN1,IN2);
-
+  //setMotor(dir,pwr,PWM,IN1,IN2);
+  forward(motor1, motor2, dir*pwr);
 
   // store previous error
   eprev = e;
 
-  Serial.print(target);
-  Serial.print(" ");
-  Serial.print(pos);
-  Serial.println();
-}
-
-void loop()
-{
-  //forward(motor1, motor2, 255);
-  
+  // Serial.print(target);
+  // Serial.print(" ");
+  // Serial.print(posA1);
+  // Serial.println();
+    // forward(motor1, motor2, 255);
     // delay(2000); // Short delay to catch your robot
-
     // motor1.drive(255, 2000);       // Turn Motor 1 for 2 seconds at full speed
     // //motor1.drive(-255,2000);
     // motor1.brake();
-    // delay(2000);
-
-    // motor2.drive(255, 2000);        // Turn Motor 2 for 2 seconds at full speed
-    // //motor2.drive(-255,2000);
-    // motor2.brake();
-    // delay(2000);
-
-    // forward(motor1, motor2, 255);        // Forward Motor 1 and Motor 2 for 1 seconds at full speed
-    // delay(1000);
-
-    // // Brake
-    // brake(motor1, motor2);     // Stop Motor 1 and Motor 2 for 2 seconds 
-    // delay(2000);     
-
-    // back(motor1, motor2, -255);         // Reverse Motor 1 and Motor 2 for 1 seconds at full speed
-    // delay(1000);
-
-    // Brake again
-    // brake(motor1, motor2);       // Stop Motor 1 and Motor 2 for 2 seconds 
-    // delay(2000);  
-
-    // left(motor1, motor2, 255);
-    // delay(2000);
-    // brake(motor1, motor2);
-    // delay(1000); 
-    // right(motor1, motor2, 255);
-    // delay(2000);
-
-    // brake(motor1, motor2);       // Stop Motor 1 and Motor 2 for 2 seconds 
-    // delay(2000); 
-
-    //Serial.println(String(posiA1) + " " + String(posiB1));
+    // forward(motor1, motor2, 255); //back(motor1, motor2, -255); // Reverse Motor 1 and Motor 2 for 1 seconds at full speed
+    // left(motor1, motor2, 255); // brake(motor1, motor2);
 }
