d8606446115dd123d7a0f266b11dbfd6ed3ad678
Azamat Tavitov <85460283+m112521@users.noreply.github.com>
2024-06-05 18:32:12 +0300

    add manual control

--- ESP32_TB6612_ENC_PID_x4.ino
+++ ESP32_TB6612_ENC_PID_x4.ino
@@ -1,4 +1,3 @@
-// #include <Arduino.h>
 #include <TB6612_ESP32.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
@@ -57,6 +56,7 @@ float eprev2 = 0;
 float eintegral = 0;
 
 int target = 0;
+String direction = "S";
 
 double kp = 1;
 double kd = 0.025;
@@ -65,6 +65,80 @@ double ki = 0.0;
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+html {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+}
+h1 {
+    font-size: 1.8rem;
+    color: white;
+}
+h2{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #143642;
+}
+.topnav {
+    overflow: hidden;
+    background-color: #143642;
+}
+body {
+    margin: 0;
+}
+.content {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+}
+.card {
+    /* box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5); */
+    padding-top:10px;
+    padding-bottom:20px;
+}
+div{
+    padding: 5px;
+}
+.button {
+    width: 100%;
+    height: 7rem;
+    padding: 15px 50px;
+    font-size: 24px;
+    text-align: center;
+    outline: none;
+    color: #fff;
+    background-color: #0f8b8d;
+    border: none;
+    border-radius: 3px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    }
+    /*.button:hover {background-color: #0f8b8d}*/
+    .button:active {
+    background-color: #0f8b8d;
+    box-shadow: 2 2px #CDCDCD;
+    transform: translateY(2px);
+    }
+    .state {
+    font-size: 1.5rem;
+    color:#8c8c8c;
+    font-weight: bold;
+    }
+    .parent {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+    }
+
+</style>
+
 <p>Target=<span id="s1"></span></p>
 <input id="target" type="number"/>
 <hr/>
@@ -81,12 +155,30 @@ const char index_html[] PROGMEM = R"rawliteral(
 
 <!-- <input id="slider" type="range" min="0" max="255"/> -->
 <button id="yo">SET</button>
+
+<hr/>
+<div class="parent">
+    <div class=""><button id="buttonLF" class="button">LF</button></div>
+    <div class=""><button id="buttonF" class="button">Forward</button></div>
+    <div class=""><button id="buttonRF" class="button">RF</button></div>
+    <div class="div2"><button id="buttonL" class="button">Left</button></div>
+    <div class="div3"><button id="buttonS" class="button">Stop</button></div>
+    <div class="div4"><button id="buttonR" class="button">Right</button></div>
+    <div class=""><button id="buttonLB" class="button">LB</button></div>
+    <div class=""><button id="buttonB" class="button">Backward</button></div>
+    <div class=""><button id="buttonRB" class="button">RB</button></div>
+    <div class=""><button id="buttonRO" class="button">Rotate</button></div>
+    <div class=""><button id="buttonRT" class="button">Rotate around Tool</button></div>
+</div>
+
 <script>
 let gateway = `ws://${window.location.hostname}/ws`;
 let target = document.querySelector("#target");
 let kp = document.querySelector("#p-textbox");
 let kd = document.querySelector("#d-textbox");
 let ki = document.querySelector("#i-textbox");
+
+let direction = "S";
 
 let websocket;
 window.addEventListener('load', onload);
@@ -97,10 +189,21 @@ function onload(event) {
 }
 
 function initButtons() {
-  document.getElementById('yo').addEventListener('click', forwardMove);
+  document.getElementById('yo').addEventListener('click', sendPID);
+  document.getElementById('buttonLF').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"LF"})) });  
+  document.getElementById('buttonF').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"F"})) });  
+  document.getElementById('buttonRF').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"RF"})) });  
+  document.getElementById('buttonL').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"L"})) });  
+  document.getElementById('buttonS').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"S"})) });  
+  document.getElementById('buttonR').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"R"})) });    
+  document.getElementById('buttonLB').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"LB"})) });  
+  document.getElementById('buttonB').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"B"})) });  
+  document.getElementById('buttonRB').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"RB"})) });  
+  document.getElementById('buttonRO').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"RO"})) });  
+  document.getElementById('buttonRT').addEventListener('click', ()=>{ websocket.send(JSON.stringify({dir:"RT"})) });  
 }
 
-function forwardMove(){
+function sendPID(){
   //websocket.send('f');
   console.log(parseFloat(ki.value));
   websocket.send(JSON.stringify({target: parseInt(target.value),kp:parseInt(kp.value),kd:parseFloat(kd.value),ki:parseFloat(ki.value)}));
@@ -201,6 +304,10 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
       // Serial.print("myObject[\"target\"] = ");
       // ledcWrite(ledChannel, brightness);
     }
+    else if (myObject.hasOwnProperty("dir")) {
+      direction = myObject["dir"];
+      manualMove(direction, 255);      
+    }
 
     String sensorReadings = getSensorReadings();
     //Serial.println(sensorReadings);
@@ -251,9 +358,54 @@ void setup()
   server.begin();
 }
 
-void loop()
-{
-  //int target = 325*sin(prevT/1e6);
+void manualMove(String direction, int speed) {
+  Serial.println(direction);
+  if (direction == "LF") {
+    forward(motor3, motor2, speed);
+  }
+  else if (direction == "F") {
+    forward(motor1, motor2, speed);
+    forward(motor3, motor4, speed);
+  }
+  else if (direction == "RF") {
+    forward(motor4, motor1, speed);
+  }
+  else if (direction == "L") {
+    forward(motor3, motor2, speed);
+    back(motor1, motor4, speed);
+  }
+  else if (direction == "S") {
+    motor1.brake();
+    motor2.brake();
+    motor3.brake();
+    motor4.brake();
+  }
+  else if (direction == "R") {
+    back(motor3, motor2, speed);
+    forward(motor1, motor4, speed);
+  }
+  else if (direction == "LB") {
+    back(motor3, motor2, speed);
+  }
+  else if (direction == "B") {
+    back(motor1, motor2, speed);
+    back(motor3, motor4, speed);
+  }
+  else if (direction == "RB") {
+    forward(motor1, motor4, speed);
+  }
+  else if (direction == "RO") {
+    forward(motor3, motor1, speed);
+    back(motor2, motor4, speed);
+  }
+  else if (direction == "RT") {
+    forward(motor3, motor3, speed);
+    back(motor4, motor4, speed);
+  }
+}
+
+void calcPID() {
+    //int target = 325*sin(prevT/1e6);
   // time difference
   long currT = micros();
   float deltaT = ((float) (currT - prevT))/( 1.0e6 );
@@ -291,6 +443,11 @@ void loop()
 
   // store previous error
   eprev = e;
+}
+
+void loop()
+{
+  //calcPID();
 
   // Serial.print(target);
   // Serial.print(" ");
